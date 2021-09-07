3473779e1c018380d4a5e648eb6164a68be17756
Riviere Fougy <rivierefougy@gmail.com>
2021-09-07 07:09:52 -0400

    store.js: created connectors

--- src/store.js
+++ src/store.js
@@ -1,10 +1,16 @@
+import axios from "axios";
 import { creatStore } from "redux";
+
+//////////////// CREATING STORE AND REDUCER FUNC ////////////////
 
 const initialState = {
   pokemonList: [],
 };
 
 const store = creatStore((state = initialState, action) => {
+  if (action.type === "LOAD") {
+    state = { ...state, pokemonList: action.pokemonList };
+  }
   if (action.type === "POST") {
     state = { ...state, pokemonList: action.pokemon };
   }
@@ -25,5 +31,36 @@ const store = creatStore((state = initialState, action) => {
     };
   }
 });
+
+//////////////// CONNECTORS ////////////////
+
+export const loadPokemon = () => {
+  return async (dispatch) => {
+    const pokemonList = (await axios.get("api/pokemon")).data;
+    dispatch({
+      type: "LOAD",
+      pokemonList,
+    });
+  };
+};
+export const updatePokemon = () => {
+  return async (dispatch) => {
+    const updatedPokemon = (await axios.put("api/pokemon/${pokemon.id}")).data;
+    dispatch({
+      type: "PUT",
+      pokemon: updatedPokemon,
+    });
+  };
+};
+export const deletePokemon = () => {
+  return async (dispatch) => {
+    const deletedPokemon = (await axios.delete("api/pokemon/${pokemon.id}"))
+      .data;
+    dispatch({
+      type: "DELETE",
+      pokemon: deletedPokemon,
+    });
+  };
+};
 
 export default store;
